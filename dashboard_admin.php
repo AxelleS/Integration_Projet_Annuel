233ab9d6fdfe754e87b3f79d88002679e98c08ca
Jérémie Noël <dev.jnoel@gmail.com>
2018-02-15 23:30:29 +0100

    setup dashboardAdmin

---
+++ dashboard_admin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard admin</title>
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+    <main class="align-front">
+      <nav class="nav-dashboard-admin">
+        <figcaption>
+          <img class="logo-dashboard" src="img/logo.png" alt="Logo Play with My CMS">
+        </figcaption>
+        <div class="nav-navbar">
+          <a href="#">
+            <div class="align-navbar-content">
+              <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+              <p>Accueil</p>
+            </div></a>
+
+          <div class="align-navbar-content">
+            <img src="https://dummyimage.com/75x75/a1a6a6/00000" alt="">
+            <p>Statistiques</p>
+          </div>
+          <div class="align-navbar-content">
+            <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+            <p>Organisation du site</p>
+          </div>
+          <div class="align-navbar-content">
+            <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+            <p>Base de données</p>
+          </div>
+          <div class="align-navbar-content">
+            <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+            <p>Calendriers</p>
+          </div>
+          <div class="align-navbar-content">
+            <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+            <p>Utilisateurs</p>
+          </div>
+          <div class="align-navbar-content">
+            <img src="https://dummyimage.com/75x75/a1a6a6/000000/fff" alt="">
+            <p>Paramètres</p>
+          </div>
+        </div>
+      </nav>
+      <header class="header-dashboard-admin">
+        <div class="logo-website">
+          <img src="" alt="">
+        </div>
+        <div class="">
+          <p>test header top</p>
+        </div>
+      </header>
+    </main>
+
+  </body>
+</html>
