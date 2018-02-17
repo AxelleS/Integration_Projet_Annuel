05c5ea79866ef9182d52f9c959b6f996d2ed6723
Jérémie Noël <dev.jnoel@gmail.com>
2018-02-17 12:40:50 +0100

    ajout dashboard-admin

---
+++ dashboard_admin.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard admin</title>
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+
+  </head>
+  <body>
+    <main class="container row">
+      <section class="align-navbar row col_3">
+        <nav class="nav-dashboard-admin">
+          <figcaption>
+            <img class="logo-dashboard" src="img/logo.png" alt="Logo Play with My CMS">
+          </figcaption>
+          <div class="nav-navbar">
+              <a href="#">
+                <div class="align-navbar-content">
+                  <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+                  <p>Accueil</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="align-navbar-content">
+                <img src="https://dummyimage.com/75x75/a1a6a6/00000" alt="">
+                <p>Statistiques</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="align-navbar-content">
+                <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+                <p>Organisation du site</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="align-navbar-content">
+                <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+                <p>Base de données</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="align-navbar-content">
+                <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+                <p>Calendriers</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="align-navbar-content">
+                <img src="https://dummyimage.com/75x75/a1a6a6/000000" alt="">
+                <p>Utilisateurs</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="align-navbar-content">
+                <img src="https://dummyimage.com/75x75/a1a6a6/000000/fff" alt="">
+                <p>Paramètres</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </section>
+      <main class="dashboard-main col_13">
+        <header class="header-dashboard-admin">
+          <div class="logo-website">
+            <img src="" alt="">
+          </div>
+          <div class="">
+            <p>test header top</p>
+          </div>
+        </header>
+
+        <div class="dashboard-title">
+          <h1>Bienvenue</h1>
+        </div>
+        <div class="dashboard-overview placement">
+          <div class="dashboard-activities col_6">
+            <p>coucou</p>
+          </div>
+          <div class="dashboard-social col_8">
+            <div class="dashboard-analytics">
+              <p>stylesheet</p>
+            </div>
+            <div class="dashboard-message">
+              <p>style2</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </main>
+
+
+
+  </body>
+</html>
