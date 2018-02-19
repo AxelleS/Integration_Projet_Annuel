c83088db202d2f57f9a4f0f7fededcfdefe20a97
AxelleS <saillyaxelle@hotmail.fr>
2018-02-19 19:05:11 +0100

    FIX

--- faq.php
+++ faq.php
@@ -2,12 +2,10 @@
 
 <main>
     <section class="row faq">
-        <div class="col_3"></div>
-        <article class="col_3 faq-picture">
+        <article class="col_3 faq-picture offset_3">
             <img src="img/faq.jpg" alt="Illustration picture of FAQ">
         </article>
-        <div class="col_2"></div>
-        <article class="col_5 faq-question">
+        <article class="col_5 offset_1 faq-question">
             <h1>FAQ</h1>
             <h4>1. Cognitis starent adpetitus mihi extremas ?</h4>
             <p>Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.</p>
