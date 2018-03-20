83e7a09783430009565023f1af2541be25b52bec
Jérémie Noël <dev.jnoel@gmail.com>
2018-03-20 13:01:06 +0100

    add users && parameters in dashboard

---
+++ controllers/ParametresController.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class ParametresController
+{
+    public function indexAction($params)
+    {
+        $v = new View('parametres','back');
+    }
+}
---
+++ controllers/UtilisateursController.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class UtilisateursController
+{
+    public function indexAction($params)
+    {
+        $v = new View('utilisateurs','back');
+    }
+}
