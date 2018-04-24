fa4b8d13562765078527e5bbf759071018a1d534
benjamin kazmierczak <kazmierczakbenjamin@gmail.com>
2018-04-24 12:51:46 +0200

    integration desktop, mobile de la page contact + avancement de reservation

--- views/reservation.view.php
+++ views/reservation.view.php
@@ -1,27 +1,29 @@
 <main>
     <section class="row resa">
-        <article class="col_16">
+        <article class="col-lg-12 col-sm-12 title-resa">
             <h1>Réserver une salle</h1>
         </article>
-        <article class="col_5 offset_3 calendar">
+        <article class="col-lg-3 col-sm-5 calendar">
             <?php include('display-calendar.php'); ?>
         </article>
-        <article class="col_1 rooms-text">
-            <p>Choix salle :</p>
-        </article>
-        <article class="col_1 rooms">
-            <select class="select-room">
-                <option value="1">Escape the Library</option>
-                <option value="2">Escape the Lab</option>
-                <option value="3">Escape the School</option>
-            </select>
-        </article>
-        <article class="col_1 slots-text">
-            <p>Choix créneau :</p>
-        </article>
-        <article class="col_1 slots">
-            <select class="creneaux"></select>
-            <?php echo '<input type="submit" value="Valider" href="'.DIRNAME.'resanext">'; ?>
-        </article>
+        <div class="row selection-resa">
+            <article class="col-lg-3 col-sm-2 rooms-text">
+                <p>Choix salle :</p>
+            </article>
+            <article class="col-lg-3 col-sm-3 rooms">
+                <select class="select-room">
+                    <option value="1">Escape the Library</option>
+                    <option value="2">Escape the Lab</option>
+                    <option value="3">Escape the School</option>
+                </select>
+            </article>
+            <article class="col-lg-6 col-sm-2 slots-text">
+                <p>Choix créneau :</p>
+            </article>
+            <article class="col-lg-3 col-sm-3 slots">
+                <select class="creneaux"></select>
+                <?php echo '<input type="submit" value="Valider" href="'.DIRNAME.'resanext">'; ?>
+            </article>
+        </div>
     </section>
 </main>
