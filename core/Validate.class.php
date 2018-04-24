e0b911305adcb2ec12fba54270b807e4a03e6dec
AxelleS <saillyaxelle@hotmail.fr>
2018-04-24 10:12:34 +0200

    Add list of route (url friendly) and token

--- core/Validate.class.php
+++ core/Validate.class.php
@@ -1,32 +1,6 @@
 <?php
 class Validate{
 
-	public static function isConnected(){
-		if((isset($_SESSION['id_user'])) && (isset($_SESSION['token']))){
-			$user = new User();
-			$user->setId($_SESSION['id_user']);
-			$response = $user->select('id');
-			$donnees_user = $response->fetch();
-	
-			if($donnees_user['token'] == $_SESSION['token']) {
-	
-				$char = 'abcdefghijklmnopqrstuvwxyz0123456789';
-				$token = str_shuffle($char);
-				$token = substr($token, 0, 11);
-				$user = new User();
-				$user->setId($_SESSION['id_user']);
-				$user->setToken($token);
-				$user->majToken();
-	
-				$_SESSION['token'] = $token;
-	
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-
 	public static function checkForm($config, $data){
 
 		$errorsMsg = [];
