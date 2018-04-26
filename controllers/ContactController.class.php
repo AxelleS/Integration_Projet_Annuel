eba27a23d897722e3dd4f58181f2e5df1702d812
AxelleS <saillyaxelle@hotmail.fr>
2018-04-26 12:41:20 +0200

    Test

--- controllers/ContactController.class.php
+++ controllers/ContactController.class.php
@@ -4,6 +4,7 @@ class ContactController
 {
     public function indexAction($params)
     {
+        echo "toto";
         //Appelle la vue
         if ($_SESSION['is_connected']) {
             $v = new View('contact','connected');
