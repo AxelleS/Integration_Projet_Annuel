956220cc028f2d7b51f316da03ab626651ce87fb
Jérémie Noël <dev.jnoel@gmail.com>
2018-05-31 15:54:47 +0200

    formFaq

---
+++ views/modals/formModifyFaq.mdl.php
@@ -0,0 +1,8 @@
+
+
+<form action="<?php echo $config['config']['action']?>"
+      class="row" method="<?php echo $config['config']['method']?>">
+      
+    <p class=""><?php echo $config['firstQuestion']['question']?></p>
+        
+      </form>
