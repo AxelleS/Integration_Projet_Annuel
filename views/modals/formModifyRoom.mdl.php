820cf874b3019404b77f6cf2d28356e3c78afb18
Jérémie Noël <dev.jnoel@gmail.com>
2018-06-21 12:31:34 +0200

    gestion 'acces room' BDD

--- views/modals/formModifyRoom.mdl.php
+++ views/modals/formModifyRoom.mdl.php
@@ -16,16 +16,17 @@
 <input class="<?php echo $config['style']['classInput']?>"  type="text" name="<?php echo $config['capacity']['name']?>" value="<?php echo $config['value']['capacity']?>" />
 
 <p class="<?php echo $config['style']['classText']?>"><?php echo $config['is_pregnant']['nameView']?></p>
-<input class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_pregnant']['name']?>" value="<?php echo $config['is_pregnant']['button_choice']['yes']['value']?>"><?php echo $config['is_pregnant']['button_choice']['yes']['name']?>
-<input class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_pregnant']['name']?>" value="<?php echo $config['is_pregnant']['button_choice']['no']['value']?>"><?php echo $config['is_pregnant']['button_choice']['no']['name']?>
+<input <?php echo ($config['is_pregnant']['is_pregnant'] === "1" ? "checked": null); ?> class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_pregnant']['name']?>" value="<?php echo $config['is_pregnant']['button_choice']['yes']['value']?>"><?php echo $config['is_pregnant']['button_choice']['yes']['name']?>
+<input <?php echo ($config['is_pregnant']['is_pregnant'] === "0" ? "checked": null); ?> class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_pregnant']['name']?>" value="<?php echo $config['is_pregnant']['button_choice']['no']['value']?>"><?php echo $config['is_pregnant']['button_choice']['no']['name']?>
 
 <p class="<?php echo $config['style']['classText']?>"><?php echo $config['is_wheelchair']['nameView']?></p>
-<input class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_wheelchair']['name']?>" value="<?php echo $config['is_wheelchair']['button_choice']['yes']['value']?>"><?php echo $config['is_pregnant']['button_choice']['yes']['name']?>
-<input class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_wheelchair']['name']?>" value="<?php echo $config['is_wheelchair']['button_choice']['no']['value']?>"><?php echo $config['is_pregnant']['button_choice']['no']['name']?>
+<input <?php echo ($config['is_wheelchair']['is_wheelchair'] === "1" ? "checked": null); ?> class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_wheelchair']['name']?>" value="<?php echo $config['is_wheelchair']['button_choice']['yes']['value']?>"><?php echo $config['is_pregnant']['button_choice']['yes']['name']?>
+<input <?php echo ($config['is_wheelchair']['is_wheelchair'] === "0" ? "checked": null); ?> class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_wheelchair']['name']?>" value="<?php echo $config['is_wheelchair']['button_choice']['no']['value']?>"><?php echo $config['is_pregnant']['button_choice']['no']['name']?>
+
 
 <p class="<?php echo $config['style']['classText']?>"><?php echo $config['is_deaf']['nameView']?></p>
-<input class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_deaf']['name']?>" value="<?php echo $config['is_deaf']['button_choice']['yes']['value']?>"><?php echo $config['is_pregnant']['button_choice']['yes']['name']?>
-<input class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_deaf']['name']?>" value="<?php echo $config['is_deaf']['button_choice']['no']['value']?>"><?php echo $config['is_pregnant']['button_choice']['no']['name']?>
+<input <?php echo ($config['is_deaf']['is_deaf'] === "1" ? "checked": null); ?> class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_deaf']['name']?>" value="<?php echo $config['is_deaf']['button_choice']['yes']['value']?>"><?php echo $config['is_pregnant']['button_choice']['yes']['name']?>
+<input <?php echo ($config['is_deaf']['is_deaf'] === "0" ? "checked": null); ?> class="<?php echo $config['style']['classInput']?>"  type="radio" name="<?php echo $config['is_deaf']['name']?>" value="<?php echo $config['is_deaf']['button_choice']['no']['value']?>"><?php echo $config['is_pregnant']['button_choice']['no']['name']?>
 
 
 <a href="<?php echo $config['config']['cancel']?>" class="<?php echo $config['style']['classCancel']?>"><button class="<?php echo $config['style']['buttonCancel']?>">Retour</button></a>
