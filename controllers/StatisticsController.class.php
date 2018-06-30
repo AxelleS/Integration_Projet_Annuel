d46bde04a1b789ac94ef4c07c45bbb4e85e84c81
benjamin kazmierczak <kazmierczakbenjamin@gmail.com>
2018-06-30 18:19:53 +0200

    index controller statistique

--- controllers/StatisticsController.class.php
+++ controllers/StatisticsController.class.php
@@ -4,7 +4,14 @@ class StatisticsController {
 
 	public function indexAction($params)
     {
+    	$user = new User();
+    	$response_stat = $user->select('id');
+    	var_dump($response_stat);exit;
+     //    $donnee_stat = $response_stat->fetch();
+
+
         $v = new View('statistics','back');
+        // $v->assign("donnees",$donnee_stat);
     }
 
     public function editAction($params){}
