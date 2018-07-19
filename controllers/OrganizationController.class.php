836891c3ecdea3941ac315cf58864649806b33db
AxelleS <saillyaxelle@hotmail.fr>
2018-07-19 12:33:02 +0200

    test

--- controllers/OrganizationController.class.php
+++ controllers/OrganizationController.class.php
@@ -166,7 +166,6 @@ class OrganizationController {
     }
 
     public function saveAction($params){
-	    print_r($params);die;
         $infoOrganization = $params['POST'];
 
         if($infoOrganization['actualPageType'] == "Homepage") {
