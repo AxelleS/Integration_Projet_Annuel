8aaae818b0a5e85bd78df51cc2dae84e1ff6c459
AxelleS <saillyaxelle@hotmail.fr>
2018-07-22 18:11:20 +0200

    FIX img + add btn share fb

--- views/escaperoom.view.php
+++ views/escaperoom.view.php
@@ -30,10 +30,8 @@
             <?php endif; ?>
 
         </article>
-        <aside class="col-lg-2 share offset-lg-1">
-            <p>Partager sur</p>
-            <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/></svg></a>
-            <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg></a>
+        <aside class="col-lg-1 share offset-lg-1">
+            <div class="fb-share-button" data-href="https://play-with-my-cms.ovh/" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fplay-with-my-cms.ovh%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>
         </aside>
     </section>
 
@@ -95,18 +93,18 @@
         </article>
         <article class="col-lg-8 col-md-6 col-sm-6 col-xs-6 picture-promotion row">
             <?php if(isset($donnees['picture_1'])) : ?>
-            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo $donnees['picture_1']; ?>" target="_blank"><img src="<?php echo $donnees['picture_1']; ?>" alt=""></a>
+            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo DIRNAME.$donnees['picture_1']; ?>" target="_blank"><img src="<?php echo DIRNAME.$donnees['picture_1']; ?>" alt=""></a>
             <?php endif; ?>
             <?php if(isset($donnees['picture_2'])) : ?>
-            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo $donnees['picture_2']; ?>" target="_blank"><img src="<?php echo $donnees['picture_2']; ?>" alt=""></a>
+            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo DIRNAME.$donnees['picture_2']; ?>" target="_blank"><img src="<?php echo DIRNAME.$donnees['picture_2']; ?>" alt=""></a>
             <?php endif; ?>
         </article>
         <article class="col-lg-8 col-md-6 col-sm-6 col-xs-6 picture-promotion row">
             <?php if(isset($donnees['picture_3'])) : ?>
-            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo $donnees['picture_3']; ?>" target="_blank"><img src="<?php echo $donnees['picture_3']; ?>" alt=""></a>
+            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo DIRNAME.$donnees['picture_3']; ?>" target="_blank"><img src="<?php echo DIRNAME.$donnees['picture_3']; ?>" alt=""></a>
             <?php endif; ?>
             <?php if(isset($donnees['picture_4'])) : ?>
-            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo $donnees['picture_4']; ?>" target="_blank"><img src="<?php echo $donnees['picture_4']; ?>" alt=""></a>
+            <a class="col-lg-6 col-md-6 col-sm-6 col-xs-6" href="<?php echo DIRNAME.$donnees['picture_4']; ?>" target="_blank"><img src="<?php echo DIRNAME.$donnees['picture_4']; ?>" alt=""></a>
             <?php endif; ?>
         </article>
     </section>
@@ -140,8 +138,15 @@
         <?php endif; ?>
     </section>
 </main>
-
 <script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.0';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
     var ladate = new Date();
     var tab_days = new Array('Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim');
     var tab_months = new Array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
@@ -176,14 +181,14 @@
             complete : function(data){
                 var timeSlots = JSON.parse(data['responseText']);
                 if(month > ladate.getMonth()){
-                    thead_title = '<img class="arrow" src="img/arrow_left.svg" onclick="removeMonth()">';
+                    thead_title = '<img class="arrow" src="<?php echo DIRNAME; ?>img/arrow_left.svg" onclick="removeMonth()">';
                 } else {
                     if (year > ladate.getFullYear()) {
-                        thead_title = '<img class="arrow" src="img/arrow_left.svg" onclick="removeMonth()">';
+                        thead_title = '<img class="arrow" src="<?php echo DIRNAME; ?>img/arrow_left.svg" onclick="removeMonth()">';
                     }
                 }
 
-                thead_title = thead_title + ' ' +tab_months[month] + ' ' + year + ' ' + '<img class="arrow" src="img/arrow_right.svg" onclick="addMonth()">';
+                thead_title = thead_title + ' ' +tab_months[month] + ' ' + year + ' ' + '<img class="arrow" src="<?php echo DIRNAME; ?>img/arrow_right.svg" onclick="addMonth()">';
                 $('#thead_title').html(thead_title);
                 var firstDay = (new Date(year, month, 1)).getDay();
                 var row = document.getElementById('tbody');
